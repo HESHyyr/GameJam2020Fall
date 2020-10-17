3856b661897faa1fcbb0503d315ef16f7654ba8c
Sheehan Ahmed <ssahmed@usc.edu>
2020-10-17 11:45:55 -0700

    environment labels, fixed door bug

---
+++ Assets/Scripts/UI/MainMenu/MainMenuOpeningAnimation.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+namespace GameJam2020
+{
+    public class MainMenuOpeningAnimation : MonoBehaviour
+    {
+
+        [SerializeField] Image bunnyIcon;
+        [SerializeField] CanvasGroup alphaMenu;
+
+        readonly AnimationCurve CURVE = AnimationCurve.EaseInOut(0f, 0f, 1f, 1f);
+
+        void Start()
+        {
+            StartCoroutine(Opener());
+        }
+
+        IEnumerator Opener()
+        {
+            bunnyIcon.rectTransform.anchoredPosition = Vector2.zero;
+            alphaMenu.alpha = 0f;
+
+            yield return new WaitForSeconds(6.0f);
+            float t = 0f;
+
+            while (t < 1.1f)
+            {
+                t += Time.deltaTime * .25f;
+                float bunnyEval = CURVE.Evaluate(t * 1.5f);
+                bunnyIcon.rectTransform.anchoredPosition = Vector3.up * 350f * bunnyEval;
+                float alphaEval = CURVE.Evaluate(t * 2f - 1f);
+                alphaMenu.alpha = alphaEval;
+                yield return null;
+            }
+
+        }
+
+    }
+}
---
+++ Assets/Scripts/UI/UIUtils/UISimpleFader.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace GameJam2020
+{
+    public class UISimpleFader : MonoBehaviour
+    {
+        [SerializeField] float delay = 3f;
+        [SerializeField] float fadeOutSpeed = 1f;
+        [SerializeField] CanvasGroup alphaGroup;
+
+        private void Start()
+        {
+            StartCoroutine(FadeCoroutine());
+        }
+
+        IEnumerator FadeCoroutine()
+        {
+            yield return new WaitForSeconds(delay);
+            float t = 0f;
+            while (t < 1.1f)
+            {
+                t += Time.deltaTime * fadeOutSpeed;
+
+                alphaGroup.alpha = 1 - t;
+
+                yield return null;
+            }
+
+            Destroy(gameObject);
+        }
+
+    }
+}
