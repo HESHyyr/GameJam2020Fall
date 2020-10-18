cdc751dc5b4d2d1ab4a00c6cab95d42fb3e415c6
Sheehan Ahmed <ssahmed@usc.edu>
2020-10-17 21:51:45 -0700

    Item.cs sprite

--- Assets/Scripts/Inventory&Items/Item.cs
+++ Assets/Scripts/Inventory&Items/Item.cs
@@ -6,12 +6,14 @@ namespace GameJam2020
     public class Item : ScriptableObject
     {
         [SerializeField] private new string name;
-        [SerializeField] private string description;
+        [SerializeField, TextArea(3, 5)] private string description;
+        [SerializeField] private Sprite sprite;
 
 
         public string Name => name;
         public string Description => description;
 
+        public Sprite Sprite => sprite;
 
     }
 }
