8620916a469a7f2f295178ff31394c7e2538c27d
Yuran Yan <yurany@uci.edu>
2020-10-18 01:29:40 -0700

    Add spite drawing to inventory, and add selection with UI

--- Assets/Scripts/UI/InventoryPanel.cs
+++ Assets/Scripts/UI/InventoryPanel.cs
@@ -3,34 +3,51 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using GameJam2020;
 
 public class InventoryPanel : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     [Header("Inventory Setting")]
     [SerializeField] private float iterationNumber;
     [SerializeField] private float iterationWaitTime;
+    [SerializeField] private Inventory inventory;
 
-    private Transform inventoryListUI;
-    private int nextAvailableInventorySlot = 0;
+    [HideInInspector] public Transform inventoryListUI;
+    [HideInInspector] public int selectedItemIndex;
+    private Transform selectionBoxTransform;
 
     
     // Start is called before the first frame update
     void Start()
     {
         inventoryListUI = transform.Find("InventoryListUI");
+        selectionBoxTransform = transform.Find("SelectionBox");
+        for (int i = 0; i < inventory.Items.Count; i++)
+            ChangeImage(i, inventory.Items[i].Sprite);
 
-        //We need to read the inventory array and draw all images here
+        selectedItemIndex = -1;
     }
 
-    public void AddItemToInventory()
+    void Update()
     {
-        //Change image parent to this transform
-        //
-        //inventoryListUI.GetChild(nextAvailableInventorySlot) = 
+        if (selectedItemIndex != -1)
+            selectionBoxTransform.position = inventoryListUI.GetChild(selectedItemIndex).position;
+            
+    }
 
+    public void AddItemToInventory(Item item)
+    {
+        inventory.AddItem(item);
+        ChangeImage(inventory.Items.Count - 1, inventory.Items[inventory.Items.Count - 1].Sprite);
+    }
 
+    public void RemoveItemFromInventory(int removeItemIndex)
+    {
+        //We need to shift the UI to 1 unit left
+        for(int i = removeItemIndex; i + 1 < inventory.Items.Count; i++)
+            ChangeImage(i, inventory.Items[i + 1].Sprite);
 
-        nextAvailableInventorySlot++;
+        inventory.RemoveItem(inventory.Items[removeItemIndex]);
     }
 
     public void OnPointerEnter(PointerEventData eventData)
@@ -52,6 +69,11 @@ public class InventoryPanel : MonoBehaviour, IPointerEnterHandler, IPointerExitH
             inventoryListUI.localPosition = inventoryListUI.localPosition + new Vector3(shouldDisplayInventory ? inventoryTransformIncreament : -inventoryTransformIncreament, 0.0f, 0.0f);
             yield return new WaitForSeconds(iterationWaitTime);
         }
+    }
+
+    private void ChangeImage(int imageIndex, Sprite resultImage)
+    {
+        inventoryListUI.GetChild(imageIndex).gameObject.GetComponent<Image>().sprite = resultImage;
     }
 
 
---
+++ Assets/Scripts/UI/ItemUI.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ItemUI : MonoBehaviour, IPointerClickHandler
+{
+    private InventoryPanel InventoryUIManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InventoryUIManager = transform.parent.parent.GetComponent<InventoryPanel>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPointerClick(PointerEventData pointerEventData)
+    {
+        if(GetComponent<Image>().sprite != null)
+        {
+            int currentItemIndex = transform.GetSiblingIndex();
+            if (InventoryUIManager.selectedItemIndex == -1)
+                InventoryUIManager.selectedItemIndex = currentItemIndex;
+            else
+            {
+                //Compare "selectedItemIndex" & "currentItemIndex" to see if they can be combined
+                if (false)
+                {
+
+                }
+                else
+                    InventoryUIManager.selectedItemIndex = currentItemIndex;
+            }
+        }
+    }
+}
